5d04368709342b5cad557187eb1ad3a16dfc1d4f
pinguxx28 <riskumax@gmail.com>
2024-09-25 07:47:45 +0300

    List.go > Get all tasks > Sort the tasks by priority then name > Print the tasks in columns

--- list.go
+++ list.go
@@ -1,9 +1,48 @@
 package main
 
-func list() {
-	// get all tasks
-	// sort in priority order
-	// sort in alphabetical order
+import (
+	"os"
+	"fmt"
+	"sort"
+	"strings"
+	"strconv"
+	"text/tabwriter"
+)
 
-	// list all tasks
+func sortTasks(tasks [][]string) {
+	sort.Slice(tasks, func(i, j int) bool {
+		// sort by priority if they are different
+		priority1, _ := strconv.Atoi(tasks[i][1])
+		priority2, _ := strconv.Atoi(tasks[j][1])
+		if priority1 != priority2 {
+			return priority1 < priority2
+		}
+
+		// sort by name if priorities are the same
+		name1 := strings.ToLower(tasks[i][2])
+		name2 := strings.ToLower(tasks[j][2])
+		return strings.Compare(name1, name2) < 0
+	})
+}
+
+func printTasks(tasks [][]string) {
+	// TODO: handle errors
+	
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.AlignRight)
+
+	for _, task := range(tasks) {
+		for i := 0; i < 3; i++ {
+			fmt.Fprintf(w, "%v\t", task[i])
+		}
+
+		fmt.Fprintln(w, "")
+	}
+
+	w.Flush()
+}
+
+func list() {
+	tasks := getAllTasks()
+	sortTasks(tasks)
+	printTasks(tasks)
 }
