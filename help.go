a736028cfb85b672bf1ddbc4cfa8d34901e09757
pinguxx28 <riskumax@gmail.com>
2024-09-29 22:20:12 +0300

    Added flags to help

--- help.go
+++ help.go
@@ -6,8 +6,8 @@ import (
 
 func help() {
 	fmt.Println("todo - a simple CLI todo list")
-	fmt.Println("Usage: todo [list|add|remove|mark|edit|help]")
-	fmt.Println("> list: lists all tasks")
+	fmt.Println("Usage: todo [list|add|remove|mark|edit|help] [FLAGS]")
+	fmt.Println("> list[a|e]: lists all tasks (a=all, e=everything)")
 	fmt.Println("> add: lets you add a new task")
 	fmt.Println("> remove: lets you remove a task")
 	fmt.Println("> mark: lets you reverse the marking of a task (completed or uncompleted)")
