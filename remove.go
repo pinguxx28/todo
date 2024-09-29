fe248bda5ad917ab778720e09ec8845898ecaef6
pinguxx28 <riskumax@gmail.com>
2024-09-29 17:15:44 +0300

    Remove.go > Get all tasks > Find task by id > Remove task > Set all new tasks

--- remove.go
+++ remove.go
@@ -1,7 +1,49 @@
 package main
 
-func remove() { 
-	// get id
+import (
+	"os"
+	"fmt"
+	"slices"
+	"encoding/csv"
+)
 
-	// remove task
+func findTaskById(tasks [][]string, id int) int {
+	for i, task := range(tasks) {
+		taskId := atoi(task[0])
+		if id == taskId {
+			return i
+		}
+	}
+
+	fmt.Printf("Couldn't find task with ID: %v\n", id)
+	os.Exit(1)
+
+	return -1 // Error if this line is not here
+}
+
+func setAllTasks(tasks [][]string) {
+	file, err := os.OpenFile("tasks.csv", os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Couldn't open file tasks.csv:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	writer.WriteAll(tasks)
+
+	if err := writer.Error(); err != nil {
+		fmt.Println("Couldn't write CSV to tasks.csv:", err)
+		os.Exit(1)
+	}
+}
+
+func remove() { 
+	tasks := getAllTasks()
+	id := atoi(getInputNum("id", 0, 999))
+
+	index := findTaskById(tasks, id)
+
+	tasks = slices.Delete(tasks, index, index + 1)
+	setAllTasks(tasks)
 }
