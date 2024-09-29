c8587df78a87f99ea5fbde38d082a31f7fff946b
pinguxx28 <riskumax@gmail.com>
2024-09-29 18:42:08 +0300

    Help.go > Display simple help page

--- help.go
+++ help.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+)
+
 func help() {
-	// print help
+	fmt.Println("todo - a simple CLI todo list")
+	fmt.Println("Usage: todo [list|add|remove|mark|edit|help]")
+	fmt.Println("> list: lists all tasks")
+	fmt.Println("> add: lets you add a new task")
+	fmt.Println("> remove: lets you remove a task")
+	fmt.Println("> mark: lets you reverse the marking of a task (completed or uncompleted)")
+	fmt.Println("> edit: lets you edit a field in a task")
+	fmt.Println("> help: displays this")
 }
